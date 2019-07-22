97e0a13fb1538469ebfaeeac5b711e1ff30e878d
kentaro_inoue <jingshang1218@gmail.com>
2019-07-22 13:05:40 +0900

    プロジェクト ファイルを追加します。

---
+++ sample190722_01/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace sample190722_01.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
